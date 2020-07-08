e196b800db7bf5cc6896c4f0c597783acd1ad243
MattT <trojanface@hotmail.com>
2020-07-08 11:33:42 +0930

    Added util scripts to do basic programmatic functions. Added components to split page up into easy to use parts. Added sort and search functionality to the employee list. Changed from using a JSON list to the randomuser.me API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; //imports react
+import './App.css'; //imports styling
+import Body from "./components/Body"; //imports the main component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Body />
     </div>
   );
 }
 
-export default App;
+export default App; //exports app to the index.js file where its rendered into index.html
